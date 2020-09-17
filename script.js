63b9ea827ec2c188b545fea7624ab0fb326f55ab
orayodeji <orayodeji@gmail.com>
2020-09-17 22:12:33 +0100

    jusy

--- script.js
+++ script.js
@@ -1,48 +1,74 @@
-const apikey = "3265874a2c77ae4a04bb96236a642d2f";
+const APIURL =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-const url = (city) =>
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+// initially get fav movies
+getMovies(APIURL);
 
-async function getWeatherByLocation(city) {
-    const resp = await fetch(url(city), { origin: "cors" });
+async function getMovies(url) {
+    const resp = await fetch(url);
     const respData = await resp.json();
 
     console.log(respData);
 
-    addWeatherToPage(respData);
+    showMovies(respData.results);
 }
 
-function addWeatherToPage(data) {
-    const temp = KtoC(data.main.temp);
-
-    const weather = document.createElement("div");
-    weather.classList.add("weather");
-
-    weather.innerHTML = `
-        <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
-        <small>${data.weather[0].main}</small>
-    `;
-
-    // cleanup
+function showMovies(movies) {
+    // clear main
     main.innerHTML = "";
 
-    main.appendChild(weather);
+    movies.forEach((movie) => {
+        const { poster_path, title, vote_average, overview } = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+            <img
+                src="${IMGPATH + poster_path}"
+                alt="${title}"
+            />
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(
+                    vote_average
+                )}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview:</h3>
+                ${overview}
+            </div>
+        `;
+
+        main.appendChild(movieEl);
+    });
 }
 
-function KtoC(K) {
-    return Math.floor(K - 273.15);
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
 }
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const city = search.value;
+    const searchTerm = search.value;
 
-    if (city) {
-        getWeatherByLocation(city);
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
     }
 });
